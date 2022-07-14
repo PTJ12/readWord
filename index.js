99164c6f67892647f5c59e2835631f8ff835b4b8
浦同矫 <15391493308@163.com>
2022-07-14 15:36:37 +0800

    修改了启动文件

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 注意 模板文件中不能出现& &读取会解析为&amp;
 const fs = require("fs");
 const AdmZip = require('adm-zip'); //引入查看zip文件的包
-const zip = new AdmZip("C:/Users/Administrator/Desktop/fc/模板文件.docx"); //filePath为文件路径
+const zip = new AdmZip("C:/Users/Administrator/Desktop/fc/读取word模板文件/模板文件.docx"); //filePath为文件路径
 let contentXml = zip.readAsText("word/document.xml");//将document.xml读取为text内容；
 let str = "";
 contentXml.match(/<w:t>[\s\S]*?<\/w:t>/ig).forEach((item) => {
